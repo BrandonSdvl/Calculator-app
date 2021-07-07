ccb1329bd1a4010f8e514f8652b2a96116a5de33
BrandonSandoval46 <brandonsandoval194@gmail.com>
2021-07-06 19:21:16 -0500

    Buttons - Design

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
 #app
   Top
+  Buttons
 </template>
 
 <script>
 import Top from "./components/Top";
+import Buttons from "./components/Buttons";
+
 export default {
   name: "App",
   components: {
     Top,
+    Buttons,
   },
 };
 </script>
---
+++ src/components/Buttons.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+.buttons
+  button.buttons__item 7
+  button.buttons__item 8
+  button.buttons__item 9
+  button.buttons__item.buttons__item--blue DEL
+  button.buttons__item 4
+  button.buttons__item 5
+  button.buttons__item 6
+  button.buttons__item +
+  button.buttons__item 1
+  button.buttons__item 2
+  button.buttons__item 3
+  button.buttons__item -
+  button.buttons__item .
+  button.buttons__item 0
+  button.buttons__item /
+  button.buttons__item x
+  button.buttons__item.buttons__item--blue RESET
+  button.buttons__item.buttons__item--red =
+</template>
+<script>
+export default {
+  name: "Buttons",
+};
+</script>
